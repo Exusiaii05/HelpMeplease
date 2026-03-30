482d0c4a371c96507c24c809d3353789db5472e6
Exusiaii05 <Devilishtaicho@gmail.com>
2026-03-30 09:37:24 +0800

    Update nltk library.py

--- nltk library.py
+++ nltk library.py
@@ -32,7 +32,7 @@ vectorizer = CountVectorizer()
 X = vectorizer.fit_transform(processed_corpus)
 
 # Train-test split
-X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=2, random_state=42)
 
 # Train a Naive Bayes classifier
 classifier = MultinomialNB()
